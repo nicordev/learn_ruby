55ed524abdaea1fe3747714f3bc0e0028cefe618
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-01-26 17:20:46 +0100

    improve functions

--- function.rb
+++ function.rb
@@ -1,5 +1,6 @@
 #! /usr/bin/ruby
 
+# Greet someone in the console
 def hello(name = 'World')
     puts "Hello #{name}!"
 end
@@ -7,3 +8,17 @@ end
 hello
 hello 'Sarah'
 hello('Jim')
+
+# Extract access token from a json response
+def extractAccessToken(keycloakToken)
+    regex = /.+\"access_token\":\"([^\"]+)\".+/m
+    matches = []
+
+    keycloakToken.scan(regex) do |match|
+        matches.push(match.to_s)
+    end
+
+    return matches[0].tr('[', '').tr(']', '') # Here we remove the characters '[' and ']'
+end
+
+puts extractAccessToken('[{"access_token":"zogzog"}]')
